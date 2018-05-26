a91be5301098c484da00f98f34b161084b525583
Saulo Machado <saulo.tracer@gmail.com>
2018-05-26 12:03:12 -0300

    Update Vetor.cs
    Removendo Coordenada origem da classe Vetor.
    Vetor não é responsável por controlar a origem, apenas define uma direção e intensidade.

--- Vetor.cs
+++ Vetor.cs
@@ -1,18 +1,15 @@
-﻿namespace Coordenadas
+namespace Coordenadas
 {
     public class Vetor
     {
         private Direcao direcao;
         private int intensidade;
-        private Coordenada origem;
 
         public Direcao Direcao { get => direcao; set => direcao = value; }
         public int Intensidade { get => intensidade; set => intensidade = value; }
-        public Coordenada Origem { get => origem; set => origem = value; }
 
-        public Vetor(Coordenada origem, Direcao direcao, int intensidade)
+        public Vetor(Direcao direcao, int intensidade)
         {
-            Origem = origem;
             Direcao = direcao;
             Intensidade = intensidade;
         }
